f55e7be39bf91a412f1776f990190da811fb1c2a
Snowsore <snow_sore@hotmail.com>
2024-10-22 15:35:47 +0800

    Add express

--- server/server.js
+++ server/server.js
@@ -1,54 +1,12 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
-const url = require("url");
+const express = require("express");
+const app = express();
 
-const hostname = "0.0.0.0";
 const port = 3000;
 
 var counter = 0;
 
-const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, "index.html");
-  const clickerFilePath = path.join(__dirname, "clicker.html");
+app.use(express.static("public"));
 
-  const parsedUrl = url.parse(req.url, true);
-  const query = parsedUrl.query;
-
-  if (parsedUrl.pathname == "/") {
-    fs.readFile(filePath, (err, data) => {
-      if (err) return;
-
-      counter++;
-
-      console.log("Query parameters:", query);
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-    return;
-  }
-
-  if (parsedUrl.pathname == "/counter") {
-    fs.readFile(filePath, (err, data) => {
-      if (err) return;
-
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ counter }));
-    });
-    return;
-  }
-
-  if (parsedUrl.pathname == "/clicker") {
-    fs.readFile(clickerFilePath, (err, data) => {
-      if (err) return;
-
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-    return;
-  }
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
 });
