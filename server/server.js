196e69a2e46b6cdd86bb9847308a3c5d724b6cdb
Snowsore <snow_sore@hotmail.com>
2024-10-22 14:55:03 +0800

    And clicker

--- server/server.js
+++ server/server.js
@@ -3,18 +3,18 @@ const fs = require("fs");
 const path = require("path");
 const url = require("url");
 
-const hostname = "127.0.0.1";
+const hostname = "0.0.0.0";
 const port = 3000;
 
 var counter = 0;
 
 const server = http.createServer((req, res) => {
   const filePath = path.join(__dirname, "index.html");
+  const clickerFilePath = path.join(__dirname, "clicker.html");
 
   const parsedUrl = url.parse(req.url, true);
   const query = parsedUrl.query;
 
-  console.log("Query parameters:", query);
 
   if (parsedUrl.pathname == "/") {
     fs.readFile(filePath, (err, data) => {
@@ -22,6 +22,7 @@ const server = http.createServer((req, res) => {
 
       counter++;
 
+      console.log("Query parameters:", query);
       res.writeHead(200, { "Content-Type": "text/html" });
       res.end(data);
     });
@@ -34,6 +35,17 @@ const server = http.createServer((req, res) => {
 
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ counter }));
+    });
+    return;
+  }
+
+  console.log(parsedUrl.pathname)
+  if (parsedUrl.pathname == "/clicker") {
+    fs.readFile(clickerFilePath, (err, data) => {
+      if (err) return;
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
     });
     return;
   }
